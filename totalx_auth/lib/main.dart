948fce166794ff53bcfc83c0753c168db089baee
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2024-04-05 09:21:35 +0530

    checking authentication

--- totalx_auth/lib/main.dart
+++ totalx_auth/lib/main.dart
@@ -34,8 +34,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // initialRoute: AuthenticationService().checklogin() ? '/userList' : '/',
-      initialRoute: '/',
+      initialRoute: AuthenticationService().checklogin() ? '/userList' : '/',
+      // initialRoute: '/',
       routes: {
         '/': (context) => LoginView(),
         '/otpview': (context) => OtpView(),
